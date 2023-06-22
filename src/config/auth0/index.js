bb444706330ca0f634551ae6711692cce50d53d2
Clinto Abraham <clinto92@gmail.com>
2023-06-22 09:09:26 +0530

    dotenv updated for production checks

--- src/config/auth0/index.js
+++ src/config/auth0/index.js
@@ -1,10 +1,12 @@
-const { AUTH0_SECRET, EXPRESS_APP_BASE_URL, AUTH0_CLIENT_ID, AUTH0_ISSUER_BASE_URL } = require('../../environments');
-  
+// const { AUTH0_SECRET, EXPRESS_APP_BASE_URL, AUTH0_CLIENT_ID, AUTH0_ISSUER_BASE_URL } = require('../../environments');
+const dotenv = require("dotenv");
+dotenv.config();
+
 exports.auth0config = {
   authRequired: false,
   auth0Logout: true,
-  secret: AUTH0_SECRET,
-  baseURL: EXPRESS_APP_BASE_URL,
-  clientID: AUTH0_CLIENT_ID,
-  issuerBaseURL: AUTH0_ISSUER_BASE_URL
-}
+  secret: process.env.AUTH0_SECRET,
+  baseURL: process.env.EXPRESS_APP_BASE_URL,
+  clientID: process.env.AUTH0_CLIENT_ID,
+  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
+};

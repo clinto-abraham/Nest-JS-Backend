2790b50c62beea6dacfb11a4ffb34a4b38520839
Clinto Abraham <clinto92@gmail.com>
2023-07-02 20:03:53 +0530

    RE-DEPLOY - try 2

--- config/config.js
+++ config/config.js
@@ -1,46 +1,46 @@
 const dotenv = require("dotenv");
-dotenv.config()
+dotenv.config();
 module.exports = {
-    "development": {
-        "username": process.env.POSTGRES_USER,
-        "password": process.env.POSTGRES_USER_PASSWORD,
-        "database": process.env.POSTGRES_DATABASE_NAME,
-        "host": "localhost",
-        "port": 5432,
-        "dialect": "postgres",
-        // Use a different storage. Default: none
-        "seederStorage": "json",
-        // Use a different file name. Default: sequelize-data.json
-        "seederStoragePath": "sequelizeData.json",
-        // Use a different table name. Default: SequelizeData
-        "seederStorageTableName": "sequelize_data"
-    },
-    "test": {
-        "username": process.env.POSTGRES_USER,
-        "password": process.env.POSTGRES_USER_PASSWORD,
-        "database": process.env.POSTGRES_DATABASE_NAME,
-        "host": "localhost",
-        "port": 5432,
-        "dialect": "postgres",
-        // Use a different storage. Default: none
-        "seederStorage": "json",
-        // Use a different file name. Default: sequelize-data.json
-        "seederStoragePath": "sequelizeData.json",
-        // Use a different table name. Default: SequelizeData
-        "seederStorageTableName": "sequelize_data"
-    },
-    "production": {
-        "username": process.env.POSTGRES_USER,
-        "password": process.env.POSTGRES_USER_PASSWORD,
-        "database": process.env.POSTGRES_DATABASE_NAME,
-        "host": "localhost",
-        "port": 5432,
-        "dialect": "postgres",
-        // Use a different storage. Default: none
-        "seederStorage": "json",
-        // Use a different file name. Default: sequelize-data.json
-        "seederStoragePath": "sequelizeData.json",
-        // Use a different table name. Default: SequelizeData
-        "seederStorageTableName": "sequelize_data"
-    }
-}
+  development: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_USER_PASSWORD,
+    database: process.env.POSTGRES_DATABASE_NAME,
+    host: "localhost",
+    port: 5432,
+    dialect: "postgres",
+    // Use a different storage. Default: none
+    seederStorage: "json",
+    // Use a different file name. Default: sequelize-data.json
+    seederStoragePath: "sequelizeData.json",
+    // Use a different table name. Default: SequelizeData
+    seederStorageTableName: "sequelize_data",
+  },
+  test: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_USER_PASSWORD,
+    database: process.env.POSTGRES_DATABASE_NAME,
+    host: "localhost",
+    port: 5432,
+    dialect: "postgres",
+    // Use a different storage. Default: none
+    seederStorage: "json",
+    // Use a different file name. Default: sequelize-data.json
+    seederStoragePath: "sequelizeData.json",
+    // Use a different table name. Default: SequelizeData
+    seederStorageTableName: "sequelize_data",
+  },
+  production: {
+    username: process.env.POSTGRES_USER_PROD,
+    password: process.env.POSTGRES_USER_PASSWORD_PROD,
+    database: process.env.POSTGRES_DATABASE_NAME_PROD,
+    host: "localhost",
+    port: 5432,
+    dialect: "postgres",
+    // Use a different storage. Default: none
+    seederStorage: "json",
+    // Use a different file name. Default: sequelize-data.json
+    seederStoragePath: "sequelizeData.json",
+    // Use a different table name. Default: SequelizeData
+    seederStorageTableName: "sequelize_data",
+  },
+};
